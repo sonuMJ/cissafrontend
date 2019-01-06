44734f37cfb993d2a0777ec4747e21c9e73d83a9
sonuMJ <sonu.sowibo@gmail.com>
2019-01-06 13:01:51 +0530

    protectroute added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch , Route } from 'react-router-dom';
+import { Switch , Route, Redirect } from 'react-router-dom';
 import Searchbar from './components/headers/Searchbar';
 import Topnav from './components/headers/Topnav';
 import Mainframe from './components/workarea/MainFrame';
 import Cartpage from './components/cart/Cartpage';
 import Cookies from 'js-cookie';
+import Confirmorder from './components/cart/Confirmorder';
+
 
 class App extends Component {
   componentDidMount(){
@@ -28,11 +30,21 @@ class App extends Component {
           {/* <Route path="/" exact addtocart component={Mainframe} /> */}
           <Route path="/cart" exact component={Cartpage}/>
           <Route path="/" exact component={Mainframe}/>
+          <PrivateRoute path='/confirmorder' component={Confirmorder} />
         </Switch>
       </div>
     );
   }
 }
-
+const fakeAuth = {
+  isAuthenticated: false
+}
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
 
 export default App;
---
+++ src/components/cart/Confirmorder.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Confirmorder extends React.Component{
+    render(){
+        return(<h1>Orders</h1>)
+    }
+}
+export default Confirmorder;
